476b5e085a13fea02832558e0ef2813daf30fed9
Saurav Gawali <gawalisaurav17@gmail.com>
2022-05-04 08:20:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/room1apr/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.room1apr;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private UserListAdapter userListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button addNewUserButton = findViewById(R.id.addNewUserButton);
+        addNewUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddNewUserActivity.class);
+                someActivityResultLauncher.launch(intent);            }
+        });
+
+        initRecyclerView();
+
+        loadUserList();
+
+
+    }
+    private void initRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        userListAdapter = new UserListAdapter(this);
+        recyclerView.setAdapter(userListAdapter);
+    }
+
+    private void loadUserList() {
+        AppDatabase db = AppDatabase.getDbInstance(this.getApplicationContext());
+        List<User> userList =db.userDao().getAllUsers();
+        userListAdapter.setUserList(userList);
+    }
+
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                        loadUserList();
+                    }
+                }
+            });
+
+}
